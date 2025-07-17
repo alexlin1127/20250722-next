f06e0206f4e6b8f435faf15cb7f994f3ddfc2e5c
tomasysh <tomasysh@gmail.com>
2025-07-17 11:21:50 +0800

    feat(Navbar): 將導航連結從 a 標籤更改為 Link 元件

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
@@ -20,14 +22,14 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ms-auto py-4 py-lg-0">
             <li className="nav-item">
-              <a className="nav-link px-lg-3 py-3 py-lg-4" href="index.html">
+              <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link px-lg-3 py-3 py-lg-4" href="about.html">
+              <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/about">
                 About
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
               <a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">
