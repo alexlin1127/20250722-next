df403dfe2c8299050dc65f550385a3ee40b51436
tomasysh <tomasysh@gmail.com>
2025-07-18 15:21:12 +0800

    feat(posts): 新增文章預覽組件以改善文章頁面結構

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,82 +1,64 @@
 import Hero from "@/components/Hero";
+import PostPreview from "./PostPreview";
+
+const POST = [
+  {
+    title: "Man must explore, and this is exploration at its greatest",
+    subTitle: "Problems look mighty small from 150 miles up",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    author: "Start Bootstrap",
+    date: "September 24, 2023",
+    id: 0,
+  },
+  {
+    title:
+      "I believe every human has a finite number of heartbeats. I don'd not intend to waste any of mine.",
+    subTitle: "",
+    author: "Start Bootstrap",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    date: "September 18, 2023",
+    id: 1,
+  },
+  {
+    title: "Science has not yet mastered prophecy",
+    subTitle:
+      "We predict too much for the next year and yet far too little for the next ten.",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    author: "Start Bootstrap",
+    date: "August 24, 2023",
+    id: 2,
+  },
+  {
+    title: "Failure is not an option",
+    subTitle:
+      "Many say exploration is part of our destiny, but it’s actually our duty to future generations.",
+    author: "Start Bootstrap",
+    content:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    date: "July 8, 2023",
+    id: 3,
+  },
+];
 
 const PostsPage = () => {
+  const posts = [...POST];
   return (
     <>
       <Hero title="Clean Blog" img="home-bg" />
       <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-md-10 col-lg-8 col-xl-7">
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 className="post-subtitle">
-                  Problems look mighty small from 150 miles up
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on September 24, 2023
-              </p>
-            </div>
-
-            <hr className="my-4" />
-
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  I believe every human has a finite number of heartbeats. I do
-                  not intend to waste any of mine.
-                </h2>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on September 18, 2023
-              </p>
-            </div>
-
-            <hr className="my-4" />
-
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">
-                  Science has not yet mastered prophecy
-                </h2>
-                <h3 className="post-subtitle">
-                  We predict too much for the next year and yet far too little
-                  for the next ten.
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on August 24, 2023
-              </p>
-            </div>
-
-            <hr className="my-4" />
-
-            <div className="post-preview">
-              <a href="post.html">
-                <h2 className="post-title">Failure is not an option</h2>
-                <h3 className="post-subtitle">
-                  Many say exploration is part of our destiny, but it’s actually
-                  our duty to future generations.
-                </h3>
-              </a>
-              <p className="post-meta">
-                Posted by
-                <a href="#!">Start Bootstrap</a>
-                on July 8, 2023
-              </p>
-            </div>
-
-            <hr className="my-4" />
-
+            {posts.map((post, index) => {
+              return (
+                <>
+                  <PostPreview post={post} key={post.id} />
+                  {index !== posts.length - 1 && <hr className="my-4" />}
+                </>
+              );
+            })}
             <div className="d-flex justify-content-end mb-4">
               <a className="btn btn-primary text-uppercase" href="#!">
                 Older Posts →
