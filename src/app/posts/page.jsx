bae46e351e075e32d533cd89ded07f22d85d0f79
tomasysh <tomasysh@gmail.com>
2025-07-18 16:17:05 +0800

    feat(about, posts): 使用 PageContainer 組件重構 AboutPage 和 PostsPage

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import PostPreview from "./PostPreview";
+import PageContainer from "@/components/PageContainer";
 
 const POST = [
   {
@@ -48,25 +49,21 @@ const PostsPage = () => {
   return (
     <>
       <Hero title="Clean Blog" img="home-bg" />
-      <div className="container px-4 px-lg-5">
-        <div className="row gx-4 gx-lg-5 justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-7">
-            {posts.map((post, index) => {
-              return (
-                <>
-                  <PostPreview post={post} key={post.id} />
-                  {index !== posts.length - 1 && <hr className="my-4" />}
-                </>
-              );
-            })}
-            <div className="d-flex justify-content-end mb-4">
-              <a className="btn btn-primary text-uppercase" href="#!">
-                Older Posts →
-              </a>
-            </div>
-          </div>
+      <PageContainer>
+        {posts.map((post, index) => {
+          return (
+            <>
+              <PostPreview post={post} key={post.id} />
+              {index !== posts.length - 1 && <hr className="my-4" />}
+            </>
+          );
+        })}
+        <div className="d-flex justify-content-end mb-4">
+          <a className="btn btn-primary text-uppercase" href="#!">
+            Older Posts →
+          </a>
         </div>
-      </div>
+      </PageContainer>
     </>
   );
 };
