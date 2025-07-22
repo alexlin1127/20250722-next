5d3ac276f85a3ada21c197387a63072bd73b8a92
Alex <Like2968@gmail.com>
2025-07-22 14:03:51 +0800

    feat: add MongoDB and Mongoose dependencies, implement database connection
    feat: create PostPreview component for displaying individual posts
    
    feat: implement PostPage component to display post details
    
    feat: create PostsPage component to list all posts with previews
    
    feat: update layout to include NextAuthProvider for authentication
    
    feat: enhance Navbar with authentication options and user profile image
    
    feat: implement NextAuthProvider for session management
    
    chore: update package.json and package-lock.json with new dependencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 
 import Footer from "@/components/Footer";
 import BootstrapProvider from "@/providers/BootstrapProvider";
+import NextAuthProvider from "@/providers/NextAuthProvider";
 import "./globals.scss";
 
 export const viewport = {
@@ -24,12 +25,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="zh-tw">
       <body>
-        <BootstrapProvider />
-        <Navbar />
-        {/* <Hero /> */}
-        {/* Hero 放在 layout，要怎麼改變標題和背景圖片？ */}
-        {children}
-        <Footer />
+        <NextAuthProvider>
+          <BootstrapProvider />
+          <Navbar />
+          {/* <Hero /> */}
+          {/* Hero 放在 layout，要怎麼改變標題和背景圖片？ */}
+          {children}
+          <Footer />
+        </NextAuthProvider>
       </body>
     </html>
   );
